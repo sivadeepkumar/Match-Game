33b1cb855e50c42214071eeae1f060753ca26f2a
sivadeepkumar <sivadeepkumar3@gmail.com>
2023-05-21 05:49:50 +0000

    Match Game

---
+++ src/NavBar/index.js
@@ -0,0 +1,30 @@
+import './index.css'
+
+const NavBar = props => {
+  const {score, sixtySecond} = props
+
+  return (
+    <nav>
+      <div className="nav-container">
+        <div>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/match-game-website-logo.png"
+            alt="website logo"
+            className="app-logo"
+          />
+        </div>
+        <div className="nav-left">
+          <p>Score : {score}</p>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/match-game-timer-img.png"
+            alt="timer"
+            className="timer-logo"
+          />
+          <p>{sixtySecond}</p>
+        </div>
+      </div>
+    </nav>
+  )
+}
+
+export default NavBar
